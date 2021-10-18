6a9b6d698a38cca092901257ca4343d8b2a40176
Louise Poubel <burajiru.no.chapulina@gmail.com>
2021-10-18 13:23:26 -0700

    Add follow tests using Ignition Gazebo's test fixture and run on GitHub actions (#40)

---
+++ .github/workflows/build-and-test.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+set -ev
+set -x
+
+# Configuration.
+export COLCON_WS=~/ws
+export COLCON_WS_SRC=${COLCON_WS}/src
+export DEBIAN_FRONTEND=noninteractive
+export ROS_PYTHON_VERSION=3
+
+apt update -qq
+apt install -qq -y lsb-release wget curl build-essential
+
+# Fortres isn't on packages.ros.org yet, so we get it from packages.osrfoundation.org
+# Once it's on packages.ros.org, it can be installed with rosdep below
+if [ "$IGNITION_VERSION" == "fortress" ]; then
+  echo "deb http://packages.osrfoundation.org/gazebo/ubuntu-stable `lsb_release -cs` main" > /etc/apt/sources.list.d/gazebo-stable.list
+  wget https://packages.osrfoundation.org/gazebo.key -O - | apt-key add -
+
+  IGN_DEPS="ignition-fortress"
+fi
+
+# Tools and dependencies
+echo "deb http://packages.ros.org/ros2/ubuntu `lsb_release -cs` main" > /etc/apt/sources.list.d/ros2-latest.list
+curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | apt-key add -
+apt-get update -qq
+apt-get install -y $IGN_DEPS \
+                   python3-colcon-common-extensions \
+                   python3-rosdep \
+                   xvfb
+
+rosdep init
+rosdep update
+
+# Create workspace and copy Dolly code there
+mkdir -p $COLCON_WS_SRC
+cp -r $GITHUB_WORKSPACE $COLCON_WS_SRC
+
+# Install ROS dependencies
+rosdep install --from-paths $COLCON_WS_SRC -i -y -r --rosdistro $ROS_DISTRO
+
+# Rendering setup
+Xvfb :1 -ac -noreset -core -screen 0 1280x1024x24 &
+export DISPLAY=:1.0
+export MESA_GL_VERSION_OVERRIDE=3.3
+
+# Build
+source /opt/ros/$ROS_DISTRO/setup.bash
+cd $COLCON_WS
+colcon build --event-handlers console_direct+
+
+# Test
+colcon test --event-handlers console_direct+ --packages-select-regex dolly
+colcon test-result
---
+++ dolly_tests/launch/follow_ignition_TEST.launch.py
@@ -0,0 +1,92 @@
+# Copyright 2021 Louise Poubel
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import os
+import unittest
+import launch_testing
+
+import launch
+import launch.actions
+import launch_testing.actions
+import launch_testing.markers
+
+from ament_index_python.packages import get_package_share_directory
+from launch_ros.actions import Node
+
+
+def generate_test_description():
+
+    # Spawn dolly
+    pkg_dolly_ignition = get_package_share_directory('dolly_ignition')
+    spawn = Node(package='ros_ign_gazebo', executable='create',
+                 arguments=[
+                    '-name', 'dolly',
+                    '-z', '0.225',
+                    '-file', os.path.join(pkg_dolly_ignition, 'models', 'dolly_ignition',
+                                          'model.sdf')],
+                 output='screen')
+
+    # Follow node
+    follow = Node(
+        package='dolly_follow',
+        executable='dolly_follow',
+        output='screen',
+        remappings=[
+            ('cmd_vel', '/dolly/cmd_vel'),
+            ('laser_scan', '/dolly/laser_scan')
+        ]
+    )
+
+    # Bridge
+    bridge = Node(
+        package='ros_ign_bridge',
+        executable='parameter_bridge',
+        arguments=['/dolly/cmd_vel@geometry_msgs/msg/Twist@ignition.msgs.Twist',
+                   '/dolly/laser_scan@sensor_msgs/msg/LaserScan@ignition.msgs.LaserScan',
+                   '/dolly/odometry@nav_msgs/msg/Odometry@ignition.msgs.Odometry'],
+        output='screen'
+    )
+
+    # Test
+    gazebo_test_fixture = Node(
+        package='dolly_tests',
+        executable='follow_ignition_TEST',
+        output='screen'
+    )
+
+    return launch.LaunchDescription([
+        gazebo_test_fixture,
+        spawn,
+        bridge,
+        follow,
+        launch_testing.util.KeepAliveProc(),
+        launch_testing.actions.ReadyToTest()
+    ]), locals()
+
+
+class DollyFollowTest(unittest.TestCase):
+
+    def test_termination(self, gazebo_test_fixture, proc_info):
+        proc_info.assertWaitForShutdown(process=gazebo_test_fixture, timeout=200)
+
+
+@launch_testing.post_shutdown_test()
+class DollyFollowTestAfterShutdown(unittest.TestCase):
+
+    def test_exit_code(self, gazebo_test_fixture, proc_info):
+        launch_testing.asserts.assertExitCodes(
+            proc_info,
+            [launch_testing.asserts.EXIT_OK],
+            gazebo_test_fixture
+        )
