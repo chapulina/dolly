753e721f1cd479cf443f5c32a32eafdbf357848e
Louise Poubel <louise@openrobotics.org>
2021-10-17 21:29:48 -0700

    Add GitHub actions CI
    Signed-off-by: Louise Poubel <louise@openrobotics.org>

---
+++ .github/workflows/build-and-test.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+set -ev
+
+# Configuration.
+export COLCON_WS=~/ws
+export COLCON_WS_SRC=${COLCON_WS}/src
+export DEBIAN_FRONTEND=noninteractive
+export ROS_PYTHON_VERSION=3
+
+apt update -qq
+apt install -qq -y lsb-release wget curl build-essential
+
+# Fortres isn't on packages.ros.org yet, so we get it from packages.osrfoundation.org
+# Once it's on packages.ros.org, it can be installed with rosdep below
+if [ "$IGNITION_VERSION" == "fortress" ]; then
+  echo "deb http://packages.osrfoundation.org/gazebo/ubuntu-stable `lsb_release -cs` main" > /etc/apt/sources.list.d/gazebo-stable.list
+  wget https://packages.osrfoundation.org/gazebo.key -O - | apt-key add -
+
+  IGN_DEPS="ignition-fortress"
+fi
+
+# Tools and dependencies
+echo "deb http://packages.ros.org/ros2/ubuntu `lsb_release -cs` main" > /etc/apt/sources.list.d/ros2-latest.list
+curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | apt-key add -
+apt-get update -qq
+apt-get install -y $IGN_DEPS \
+                       python3-colcon-common-extensions \
+                       python3-rosdep
+
+rosdep init
+rosdep update
+rosdep install --from-paths ./ -i -y -r --rosdistro $ROS_DISTRO
+
+# Build
+source /opt/ros/$ROS_DISTRO/setup.bash
+mkdir -p $COLCON_WS_SRC
+cp -r $GITHUB_WORKSPACE $COLCON_WS_SRC
+cd $COLCON_WS
+colcon build --event-handlers console_direct+
+
+# Test
+colcon test --event-handlers console_direct+
+colcon test-result
